d4f300b93daf6f9f064c85664c183bc0c3bd9ad9
Tyler Edlin <edlin.michael1@gmail.com>
2024-06-25 18:19:33 -0400

    Adds coding nomad python bootcamp

---
+++ codingnomads/python-101-main/05_machine-setup/05_04_seconds_year.py
@@ -0,0 +1,14 @@
+# Use the interpreter to calculate how many seconds are in a year.
+# Then write the code in this script file below this line.
+
+# Define the numbers
+seconds_in_a_minute = 60
+minutes_in_a_hour = 60
+hours_in_a_day = 24
+days_in_a_year = 365
+
+# calculate the total number of seconds in a year
+seconds_in_a_year = seconds_in_a_minute * minutes_in_a_hour * hours_in_a_day * days_in_a_year
+
+# Print the result
+print("There are", seconds_in_a_year, "seconds in the year.")
---
+++ codingnomads/python-101-main/06_intro-to-programming/06_01_pseudocode.py
@@ -0,0 +1,12 @@
+# Your cat just had kittens! Now you want to put up an ad to give them
+# to your friends. You'll need to save all names of the kittens,
+# confirm that they each of them is cute, and show a message that
+# that kitten is ready for adoption.
+#
+# Break this task up into a couple of steps of pseudocode
+# and write the pseudocode below in code comments.
+# You don't need to write any functional code, just map out the steps.
+
+# create an array of kitten names
+# create a boolean to define in they are cute or not
+# attach a string for the description that the kitten is ready for adoption
